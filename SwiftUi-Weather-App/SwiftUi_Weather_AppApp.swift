5c8de2461a0295a0429f72cb2a3513b943d6dcf9
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-12-16 14:57:17 +0600

    Initial Commit

---
+++ SwiftUi-Weather-App/SwiftUi_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_Weather_AppApp.swift
+//  SwiftUi-Weather-App
+//
+//  Created by Hadiuzzaman on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
