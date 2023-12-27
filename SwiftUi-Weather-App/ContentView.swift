484dbbcedbab06f7a7119c41b4cf99a7ed10cb44
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-12-27 23:09:53 +0600

    refacor: split view

--- SwiftUi-Weather-App/ContentView.swift
+++ SwiftUi-Weather-App/ContentView.swift
@@ -10,39 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+            BackgroundView()
             VStack{
-                Text("Dhaka, Bangladesh").font(.system(size: 30,weight: .medium, design:.monospaced)).foregroundColor(Color.white).padding()
-                VStack{
-                    Image(systemName: "cloud.sun.fill").renderingMode(.original).resizable().aspectRatio( contentMode:.fit).frame(width: 180, height: 180)
-                }
-                
-                Text("76°").font(.system(size: 70,weight: .medium))
-                
+                CityNameView(cityName: "Dhaka, Bangladesh")
+               
+                WeatherSummary()
+                   
                 HStack{
-                    VStack{
-                        Text("Sat").font(.system(size: 24))
-                        Image(systemName: "cloud.sun.rain.fill").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                    }
-                    VStack{
-                        Text("Sat").font(.system(size: 24))
-                        Image(systemName: "cloud.sun.rain.fill").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                    }
-                    VStack{
-                        Text("Sat").font(.system(size: 24))
-                        Image(systemName: "cloud.sun.rain.fill").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                    }
-                    VStack{
-                        Text("Sat").font(.system(size: 24))
-                        Image(systemName: "cloud.sun.rain.fill").renderingMode(.original).resizable().frame(width: 50, height: 50)
-                    }
+                    DayWiseWeatherCondition(dayName: "Sat", iconName: "cloud.sun.fill")
+                    DayWiseWeatherCondition(dayName: "Sun", iconName: "cloud.sun.fill")
+                    DayWiseWeatherCondition(dayName: "Mon", iconName: "cloud.sun.fill")
+                    DayWiseWeatherCondition(dayName: "Thu", iconName: "cloud.sun.fill")
+                    DayWiseWeatherCondition(dayName: "Wed", iconName: "cloud.sun.fill")
+                
         
                 }
                 Spacer()
                 Button {
                     print("tapped")
                 } label: {
-                    Text("Refresh Weather").frame(width: 280,height: 60).background(.white).cornerRadius(10).font(.system(size: 18,weight: Font.Weight.bold))
+                    Text("Refresh Weather").frame(width: 280,height:60).background(.white).cornerRadius(10).font(.system(size: 18,weight: Font.Weight.bold))
                 }
                 
                     Spacer()
@@ -55,5 +42,41 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        LinearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityNameView: View {
+    var cityName:String
+    var body: some View {
+        Text("\(cityName)").font(.system(size: 30,weight: .medium, design:.monospaced)).foregroundColor(Color.white).padding()
+    }
+}
+
+struct WeatherSummary: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "cloud.sun.fill").renderingMode(.original).resizable().aspectRatio( contentMode:.fit).frame(width: 180, height: 180)
+            Text("76°").font(.system(size: 70,weight: .medium))
+        }
+     
+    }
+}
+
+struct DayWiseWeatherCondition: View {
+    
+    var dayName:String
+    var iconName:String
+    
+    var body: some View {
+        VStack{
+            Text("\(dayName)").font(.system(size: 24))
+            Image(systemName: "\(iconName)").renderingMode(.original).resizable().frame(width: 50, height: 50)
+        }
     }
 }
